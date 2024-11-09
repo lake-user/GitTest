131ae82d70ae479c2e21a07574ae0db43b5a8d50
lake-user <108962567+lake-user@users.noreply.github.com>
2024-11-09 21:35:11 +0800

    Add files via upload

---
+++ HelloWorld.cpp
@@ -0,0 +1,7 @@
+#include<iostream>
+using namespace std;
+int main(){
+    cout<<"Hello World!"<<endl;
+}
+//vscode配置c++环境
+//https://blog.csdn.net/thefg/article/details/137474276?spm=1001.2101.3001.6650.4&utm_medium=distribute.pc_relevant.none-task-blog-2%7Edefault%7EBlogCommendFromBaidu%7ECtr-4-137474276-blog-78001882.235%5Ev43%5Epc_blog_bottom_relevance_base4&depth_1-utm_source=distribute.pc_relevant.none-task-blog-2%7Edefault%7EBlogCommendFromBaidu%7ECtr-4-137474276-blog-78001882.235%5Ev43%5Epc_blog_bottom_relevance_base4&utm_relevant_index=9
